15cf3982c813a61cd0990be600e9010158dd55a5
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-04-30 10:47:29 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.69 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,23 @@
+function solution(arr1, arr2) {
+    const r1 = arr1.length;
+    const c1 = arr1[0].length;
+    
+    const r2 = arr2.length;
+    const c2 = arr2[0].length;
+    
+    const result = [] 
+    
+    for(let i=0; i<r1; i+=1){
+        result.push(new Array(c2).fill(0));
+    }
+    
+    for(let i=0; i<r1; i+=1){
+        for(let j=0; j<c2; j+=1){
+            for(let k=0; k<c1; k+=1){
+                result[i][j] += arr1[i][k]*arr2[k][j];
+            }
+        }
+    }
+    
+    return result;
+}

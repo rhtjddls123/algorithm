cda3d522569479a59fcbc12a9b8d64c8d202d4f6
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-01-05 17:51:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.92 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.91 ms
+메모리: 33.7 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 30일 09:50:07
+2026년 01월 05일 17:51:55
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,20 @@
 function solution(numbers) {
-    const arr=[]
+    let num;
+    const result = []
     for(let i=0; i<numbers.length-1; i+=1){
         for(let j=i+1; j<numbers.length; j+=1){
-            arr.push(numbers[i]+numbers[j])
+            num = numbers[i]+numbers[j]
+            result.push(num)
         }
     }
     
-    const setArr = [...new Set(arr)];
-    const sortedArr = setArr.sort((a,b)=>a-b);
-    return sortedArr;
+    return [...new Set(result)].sort((a,b)=>a-b)
 }
+
+// 정수 배열이 주어진다
+// 해당 배열에서 서로 다른 인덱스의 두 수를 뽑는다
+// 두 수를 더해 만들 수 있는 모든 수를 새 배열에 오름차순으로 담는다
+
+// 어떤 수가 나올지는 모르기 때문에 모든 인덱스 쌍을 확인해야할것으로 보임
+// 이중 for문을 사용하여 비교
+// 대신에 내부 for문의 시작 index는 외부 for문의 다음

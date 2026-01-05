a4174c2eaaef5264689ce7fbb1dd3af6dc6626c5
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-01-05 18:15:01 +0900

    [level 1] Title: 모의고사, Time: 30.07 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 30.20 ms
+메모리: 36.8 MB, 시간: 30.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 30일 10:20:22
+2026년 01월 05일 18:14:59
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,21 +3,28 @@ function solution(answers) {
     const second = [2,1,2,3,2,4,2,5];
     const third = [3,3,1,1,2,2,4,4,5,5];
     
-    const result = [0,0,0]
+    const score = [0,0,0];
+    
     for(let i=0; i<answers.length; i+=1){
-        answer = answers[i];
-        result[0] += answer===first[i%first.length]?1:0;
-        result[1] += answer===second[i%second.length]?1:0;
-        result[2] += answer===third[i%third.length]?1:0;
+        const answer = answers[i];
+        score[0] += first[i%first.length]===answer?1:0
+        score[1] += second[i%second.length]===answer?1:0
+        score[2] += third[i%third.length]===answer?1:0
     }
     
-    const max = Math.max(...result);
+    const max = Math.max(...score);
+    const result = [];
     
-    const a = [];
-    for(let i=0; i<result.length; i+=1){
-        if(result[i]===max){
-            a.push(i+1)
-        }
+    for(let i=0; i<score.length; i+=1){
+        if(score[i]===max) result.push(i+1)
     }
-    return a
+    
+    return result;
 }
+// 현재 문제의 정답이 각 찍는 방식에 부합하면 1씩 더해야됨
+
+// 1,2,3번 수포자가 찍는 방식이 모두 다름
+// 정답에 대해 1,2,3번이 각각 몇개씩 맞췄나 저장 후, 비교해야 함
+// 그렇다면 각 수포자에 대해 index별로 정답인지를 확인해볼까?
+// 예를들어 1번 수포자는 index값이 0,5,10,15... 처럼 index%5=0일 때 answers[index] = 1이면 정답
+// 근데 문제 수가 그리 많지 않아서 그냥 for문을 돌리면서 확인해도 될거같긴 한데..

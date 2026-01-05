2006ddf91120396b8d076eeec751a66be3ea5bc0
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-01-05 19:33:41 +0900

    [level 1] Title: 실패율, Time: 692.17 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 40.4 MB, 시간: 3.20 ms
+메모리: 43.4 MB, 시간: 692.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 30일 11:17:32
+2026년 01월 05일 19:33:39
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,20 +1,33 @@
 function solution(N, stages) {
-    let P = stages.length;
-    const result = new Array(N).fill(0)
-
-    for(let i=0; i<stages.length; i+=1){
-        const stage = stages[i];
-        if(stage-1<N)
-            result[stage-1] += 1
+    const rate = {}
+    for(let i=1; i<=N; i+=1){
+        let allPlayers = 0
+        for(let j=0; j<stages.length; j+=1){
+            if(stages[j]>=i) allPlayers+=1;
+        }
+        
+        if(allPlayers===0) {
+            rate[i] = 0;
+            continue;
+        }
+        
+        const failPlayers = stages.filter(a=>a===i).length;
+        rate[i] = failPlayers/allPlayers;
     }
     
-    const rat = []
-    for(let i=0; i<result.length; i+=1){
-        rat.push([i+1,result[i]/P])
-        P-=result[i]
-    }
-    
-    rat.sort((a,b)=>b[1]-a[1])
-    
-    return rat.map((v)=>v[0])
+    const result = Object.entries(rate).sort((a,b)=>b[1]-a[1])
+    return result.map(v=>Number(v[0]))
 }
+
+// 실패율 = 스테이지에서 클리어 못한 플레이어 수 / 스테이지 도달 플레이어 수
+// N = 스테이지 개수, stages = 사용자가 현재 멈춰있는 스테이지 배열
+
+// 특정 스테이지의 실패율 구하기
+// 1. 특정 스테이지에 도달한 플레이어 수를 먼저 구함
+//    그러기 위해서는 특정 스테이지보다 사용자의 스테이지가 큰 값을 모두 구하기
+//    예를들어 1스테이지의 총 도달 플레이어 수는 stages배열에서 1보다 크거나 같은 값들의 개수
+//    만약 도달한 플레이어가 없으면 실패율은 바로 0
+// 2. 스테이지에서 클리어 못한 플레이어의 수 구하기
+//    해당 값은 특정 스테이지의 사용자 수를 구하면 됨
+//    예를들어 1스테이지를 클리어 못한 플레이어는 stages배열에서 1인 값들의 개수
+// 3. 해당 실패율을 각각의 값에 저장 후 높은 순서대로 스테이지를 반환하기

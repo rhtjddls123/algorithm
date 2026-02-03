8a4d5a66dc1d2d11c278906bb16de5c4f40d4d53
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-02-04 00:27:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 137.13 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,7 @@
 function solution(participant, completion) {
     const pObj = {}
     for(const p of participant){
-        pObj[p] = pObj[p]!==undefined ? pObj[p]+1 : 0
+        pObj[p] = pObj[p] ? pObj[p]+1 : 1
     }
     
     for(const c of completion){
@@ -9,7 +9,7 @@ function solution(participant, completion) {
     }
     
     for(const [k,v] of Object.entries(pObj)){
-        if(v===0){
+        if(v===1){
             return k
         }
     }

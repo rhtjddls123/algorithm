2d2a332a2c38870412f8677eeb0db7ddedbe70a1
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-03-09 22:29:36 +0900

    [level 3] Title: 여행경로, Time: 631.77 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,43 @@
+function solution(tickets) {
+  const obj = {};
+  const result = ["ICN"];
+  for (const ticket of tickets) {
+    const from = ticket[0];
+    const to = ticket[1];
+
+    if (obj[from]) {
+      obj[from].push(to);
+    } else {
+      obj[from] = [to];
+    }
+
+    obj[from].sort();
+  }
+
+  const results = [];
+
+  function dfs(obj, from) {
+    const copyObj = JSON.parse(JSON.stringify(obj));
+
+    if (result.length === tickets.length + 1) {
+      results.push([...result]);
+      return;
+    }
+
+    if (!copyObj[from] || !copyObj[from].length) {
+      return;
+    }
+
+    for (let i = 0; i < copyObj[from].length; i += 1) {
+      const to = copyObj[from].pop();
+
+      result.push(to);
+      dfs({ ...copyObj }, to);
+      result.pop();
+      copyObj[from].unshift(to);
+    }
+  }
+
+  dfs(obj, "ICN");
+  return results.pop()
+}

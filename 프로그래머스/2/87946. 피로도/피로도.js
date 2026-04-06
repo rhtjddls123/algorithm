602c26b2f1e2b67543585da35a204f55dec473fe
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-06 19:12:20 +0900

    [level 2] Title: 피로도, Time: 194.51 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,22 +1,18 @@
 function solution(k, dungeons) {
-  const visited = Array.from({ length: dungeons.length }).fill(0);
-
-  function backtrack(curK, cnt) {
-    let max = cnt;
-
-    for (let i = 0; i < dungeons.length; i += 1) {
-      if (dungeons[i][0] <= curK && visited[i] === 0) {
-        visited[i] = 1;
-
-        max = Math.max(max, backtrack(curK - dungeons[i][1], cnt + 1));
-
-        visited[i] = 0;
-      }
+    const visited = Array.from({length: dungeons.length}, ()=>0);
+    
+    function backtracking(curK, cnt){
+        let max = cnt;
+        for(let i=0; i<visited.length; i+=1){
+            if(visited[i]===1 || dungeons[i][0]>curK) continue;
+            
+            visited[i] = 1;
+            max = Math.max(max, backtracking(curK-dungeons[i][1], cnt+1));
+            visited[i] = 0;
+        }
+        return max;
     }
-
+    
+    const max = backtracking(k, 0);
     return max;
-  }
-
-  let max = backtrack(k, 0);
-  return max;
 }

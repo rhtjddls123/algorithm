4e371cc4c40081ab99ca730350888a4b97415a7b
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-09 22:55:41 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.54 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    result = []
+    
+    for c in course:
+        counter = Counter()
+        
+        for order in orders:
+            for combo in combinations(sorted(order), c):
+                counter[''.join(combo)]+=1
+        
+        if not counter: continue
+        
+        max_count = max(counter.values())
+        
+        if max_count>=2:
+            for menu in counter.items():
+                if (menu[1] == max_count):
+                    result.append(menu[0])
+                    
+    return sorted(result)

fcb11850b1be1eb9e42eba43ab49c3868a5c4938
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-14 13:15:34 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.26 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -3,13 +3,12 @@ from collections import Counter
 
 def solution(orders, course):
     result = []
-    
     for c in course:
         counter = Counter()
         
         for order in orders:
             for combo in combinations(sorted(order), c):
-                counter[''.join(combo)]+=1
+                counter[''.join(combo)] += 1
         
         if not counter: continue
         
@@ -17,7 +16,6 @@ def solution(orders, course):
         
         if max_count>=2:
             for menu in counter.items():
-                if (menu[1] == max_count):
+                if menu[1]==max_count:
                     result.append(menu[0])
-                    
     return sorted(result)

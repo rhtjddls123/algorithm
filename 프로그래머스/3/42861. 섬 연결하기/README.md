154fc71aa66db35a065155229c828f9522909f2d
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-16 12:56:52 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 0.09 ms
+메모리: 9.17 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 15일 19:23:45
+2026년 04월 16일 12:56:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,21 +1,33 @@
 def solution(n, costs):
-    arr = [x for x in range(n)]
-    result = 0
     costs = sorted(costs, key=lambda x:x[2])
+    set_arr = [i for i in range(n)]
+    rank = [0]*n
+    result = 0
+    
     for cost in costs:
-        if (union(arr, cost[0], cost[1])):
+        if(union(set_arr, rank, cost[0], cost[1])):
             result += cost[2]
+    
     return result
-    
-def find(arr, x):
-    if(arr[x]==x):
-        return x
-    
-    return find(arr, arr[x])
 
-def union(arr, x, y):
+def find(arr, x):
+    if(arr[x] != x):
+        arr[x] = find(arr, arr[x])
+    return arr[x]
+
+def union(arr, rank, x, y):
     root_x = find(arr, x)
     root_y = find(arr, y)
-    if root_x != root_y:
-        arr[root_y] = root_x
-    return root_x != root_y
+    
+    if root_x == root_y:
+        return False
+    
+    if rank[root_x] < rank[root_y]:
+        root_x, root_y = root_y, root_x
+        
+    arr[root_y] = root_x
+    
+    if rank[root_x] == rank[root_y]:
+        rank[root_x] += 1
+    
+    return True

06cb18839d02653b4ed197f2e72498fb1e3559b8
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-16 14:39:31 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,33 +1,15 @@
-from collections import Counter
-
 def solution(n, computers):
-    set_arr = [i for i in range(n)]
-    rank = [0]*n
+    visited = [False] * n
+    result = 0
     for i in range(n):
-        for j in range(n):
-            if computers[i][j]==1:
-                union(set_arr, rank, i, j)
+        if visited[i]==False:
+            dfs(n, computers, visited, i)
+            result += 1
+    return result
     
+def dfs(n, computers, visited, node):
+    visited[node] = True
     for i in range(n):
-        find(set_arr, i)
-    counter = Counter(set_arr)
-    return len(counter)
-
-def find(arr, x):
-    if arr[x]!=x:
-        arr[x]=find(arr,arr[x])
-    return arr[x]
-
-def union(arr, rank, x, y):
-    root_x = find(arr,x)
-    root_y = find(arr,y)
-    
-    if root_x == root_y:
-        return False
-    
-    if rank[root_x]<rank[root_y]:
-        root_x, root_y = root_y, root_x
-    arr[root_y] = root_x
-    if rank[root_x] == rank[root_y]:
-        rank[root_x] += 1
-    return True
+        if computers[i][node] == 1 and visited[i]==False:
+            dfs(n, computers, visited, i)
+        

db1d78d74b15df744c3f5fb88b7671383c3433ce
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-19 16:05:59 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,18 +1,17 @@
 def solution(n, costs):
-    costs = sorted(costs, key=lambda x:x[2])
     set_arr = [i for i in range(n)]
     rank = [0]*n
+    costs = sorted(costs, key=lambda x:x[2])
     result = 0
-    
     for cost in costs:
-        if(union(set_arr, rank, cost[0], cost[1])):
+        if union(set_arr, rank, cost[0], cost[1]):
             result += cost[2]
-    
     return result
 
 def find(arr, x):
-    if(arr[x] != x):
+    if arr[x] != x:
         arr[x] = find(arr, arr[x])
+    
     return arr[x]
 
 def union(arr, rank, x, y):
@@ -23,11 +22,8 @@ def union(arr, rank, x, y):
         return False
     
     if rank[root_x] < rank[root_y]:
-        root_x, root_y = root_y, root_x
-        
+        root_x, root_y = root_y, root_x    
     arr[root_y] = root_x
-    
     if rank[root_x] == rank[root_y]:
         rank[root_x] += 1
-    
     return True

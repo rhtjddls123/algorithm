d546382e1abecbc9bc3674fb2c249506780fbfc3
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-04-27 19:38:01 +0900

    [level 2] Title: 배달, Time: 2.48 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -3,26 +3,25 @@ import heapq
 
 def solution(N, road, K):
     nodes = defaultdict(list)
-    for f_node, s_node, weight in road:
-        nodes[f_node].append((s_node, weight))
-        nodes[s_node].append((f_node, weight))
-    visited = [False] * (N+1)
-    distance = [float('inf')] * (N+1)
-    distance[1] = 0
-    queue = [(0, 1)]
+    for n1, n2, weight in road:
+        nodes[n1].append((n2, weight))
+        nodes[n2].append((n1, weight))
+    visited = [False]*(N+1)
+    costs = [float('inf')]*(N+1)
+    costs[1] = 0
+    queue = [(0,1)]
     
     while queue:
         cur_weight, cur_node = heapq.heappop(queue)
         if visited[cur_node]: continue
         visited[cur_node] = True
-        
-        for next_node, next_weight in nodes[cur_node]:
-            new_weight = cur_weight + next_weight
-            if distance[next_node] > new_weight:
-                distance[next_node] = new_weight
-                heapq.heappush(queue, (new_weight, next_node))
+        for next_node, next_cost in nodes[cur_node]:
+            new_cost = cur_weight + next_cost
+            if new_cost < costs[next_node]:
+                costs[next_node] = new_cost
+                heapq.heappush(queue, (new_cost, next_node))
     
     result = 0
-    for cost in distance:
-        if cost<=K: result += 1
+    for i in costs:
+        if i<=K: result+=1
     return result

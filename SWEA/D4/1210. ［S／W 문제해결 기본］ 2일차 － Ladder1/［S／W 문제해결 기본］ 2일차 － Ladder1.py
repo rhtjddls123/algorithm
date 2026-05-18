2d11b72c2a8382a9fd9db611048693063d852634
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-05-18 22:10:23 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 119 ms, Memory: 61,696 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,29 +1,28 @@
+from collections import defaultdict
+
 for test_case in range(1, 11):
-    n = int(input())
-    arr = []
-    size = 100
-    for _ in range(size):
-      row = list(map(int, input().split()))
-      arr.append(row)
-  
-    start = [size-1, arr[size-1].index(2)] # 목표 위치, 아래부터 올라갈것 높이, 가로
-    
-    while(True):
-      if(start[0]==0):
-        break
-      
-      # 맨 아래부터 위로
-      if(start[1]>0 and arr[start[0]][start[1]-1]==1):
-        # 왼쪽에 길이 있을 경우
-        arr[start[0]][start[1]] = 0
-        start[1] -= 1
-      elif(start[1]<size-1 and arr[start[0]][start[1]+1]==1):
-        # 오른쪽에 길이 있을 경우
-        arr[start[0]][start[1]] = 0
-        start[1] += 1
-      else:
-        arr[start[0]][start[1]] = 0
-        start[0] -= 1
-        
-    
-    print(f'#{n} {start[1]}')
+    T = int(input())
+    map_arr = [list(map(int, input().split())) for _ in range(100)]
+    goal = map_arr[99].index(2)
+    dir = [(0,1),(0,-1),(-1,0)]
+    visited = [[False] * 100 for _ in range(100)]
+
+    stack = [(99, goal)]
+    result = -1
+    while stack:
+        x, y = stack.pop()
+        if x==0:
+            result = y
+            break
+        if visited[x][y]: continue
+
+        visited[x][y] = True
+        for d in dir:
+            dx, dy = x+d[0], y+d[1]
+
+            if 0<=dx<100 and 0<=dy<100 and map_arr[dx][dy]!=0 and not visited[dx][dy]:
+                stack.append((dx, dy))
+                break
+
+    print(f'#{T} {result}')
+

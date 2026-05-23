a1cf38da8de5721f8e3562705cfedbf5e9c189fd
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-05-23 14:09:25 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 111 ms, Memory: 61,184 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,24 +1,21 @@
-for test_case in range(1, 11):
-    n = int(input())
-    arr = []
-    result = [[],[0 for i in range(100)],[0,0]]
-    for _ in range(100):
-      row = list(map(int, input().split()))
-      result[0].append(sum(row))
-      arr.append(row)
-    
-    for i in range(len(arr)):
-       a = arr[i]
-       for j in range(len(a)):
-          result[1][j] += a[j]
-          if(i==j):
-             result[2][0] += a[i]
-          elif(i+j==99):
-             result[2][1] += a[i]
-    
-    m = 0
-    for r in result:
-       if(m<max(r)):
-          m = max(r)
+for _ in range(10):
+    test_case = int(input())
+    n = 100
+    nums = [list(map(int, input().split())) for _ in range(n)]
+    a, b, c = 0, 0, 0
+    for r in nums:
+        a = max(a, sum(r))
+    for i in range(n):
+        tmp = 0
+        for j in range(n):
+            tmp += nums[j][i]
+        b = max(b, tmp)
 
-    print(f'#{n} {m}')
+    tmp_a = 0
+    tmp_b = 0
+    for i in range(n):
+        for j in range(n):
+            if i==j: tmp_a+=nums[i][j]
+            if i+j==n-1: tmp_b+=nums[i][j]
+    c = max(tmp_a, tmp_b)
+    print(f'#{test_case} {max(a,b,c)}')

fcd6f7aa9390aa40bc56dc6f1e12c54b5b1bf06e
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-08-12 11:41:09 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,50 +1,27 @@
 import java.util.*;
 
 class Solution {
-    int[] nodes;
+    boolean[] visited;
 
     public int solution(int n, int[][] computers) {
-        nodes = new int[n];
+        visited = new boolean[n];
+        int answer = 0;
 
         for (int i = 0; i < n; i++) {
-            nodes[i] = i;
-        }
-
-        List<int[]> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j=0; j < n; j++) {
-                if (computers[i][j] == 1) {
-                    list.add(new int[]{i, j});
-                }
+            if(!visited[i]) {
+                dfs(i, computers);
+                answer++;
             }
         }
-
-        for (int[] ints : list) {
-            union(ints[0], ints[1]);
-        }
-        
-        Set<Integer> set = new HashSet<>();
-        for (int node : nodes) {
-            set.add(find(node));
-        }
-
-        int answer = set.size();
         return answer;
     }
 
-    public int find(int x) {
-        if (nodes[x] != x) {
-            nodes[x] = find(nodes[x]);
-        }
-        return nodes[x];
-    }
-
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if(rootX!=rootY) {
-            nodes[rootY] = rootX;
+    public void dfs(int i, int[][] computers) {
+        visited[i] = true;
+        for (int j = 0; j < computers[i].length; j++) {
+            if (computers[i][j]==1 && !visited[j]) {
+                dfs(j, computers);
+            }
         }
     }
 }
